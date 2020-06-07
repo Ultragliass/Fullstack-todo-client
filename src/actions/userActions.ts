7e69c52cd1742724c0bf6fb0ce3c00731db5acee
Michael H <MichaelHahmigery@gmail.com>
2020-06-07 16:50:55 +0300

    Create user register logic.

--- src/actions/userActions.ts
+++ src/actions/userActions.ts
@@ -5,7 +5,10 @@ import { Actions } from "../redux/reducer";
 
 const BASE_LINK: string = "http://localhost:3001";
 
-export const loginUserAction = (username: string, password: string) => {
+export const loginUserAction = (
+  username: string,
+  password: string
+): Function => {
   return async (dispatch: Dispatch<IAction>) => {
     try {
       const response = await axios.post(`${BASE_LINK}/login`, {
@@ -19,20 +22,49 @@ export const loginUserAction = (username: string, password: string) => {
 
       dispatch({
         type: Actions.loginUser,
+        payload: {},
+      });
+    } catch (err) {
+      const error = err.response.data.msg;
+
+      dispatch({
+        type: Actions.showError,
         payload: {
-          token,
+          error,
         },
       });
+    }
+  };
+};
 
-    } catch (response) {
-        const error = response.data.msg;
+export const registerUserAction = (
+  username: string,
+  password: string
+): Function => {
+  return async (dispatch: Dispatch<IAction>) => {
+    try {
+      const response = await axios.post(`${BASE_LINK}/register`, {
+        username,
+        password,
+      });
 
-        dispatch({
-            type: Actions.showError,
-            payload: {
-                error
-            }
-        });
+      const { token } = response.data;
+
+      localStorage.setItem("token", token);
+
+      dispatch({
+        type: Actions.loginUser,
+        payload: {},
+      });
+    } catch (err) {
+      const error = err.response.data.msg;
+
+      dispatch({
+        type: Actions.showError,
+        payload: {
+          error,
+        },
+      });
     }
   };
 };
