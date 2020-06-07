dd36f85ff24c40a115fd72d9e86a7d881b98ab58
Michael H <MichaelHahmigery@gmail.com>
2020-06-07 17:40:06 +0300

    Create react router path for login page and add fallback.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./App.css";
 import { Container } from "react-bootstrap";
+import { Switch, Route, Redirect, Router } from "react-router-dom";
 
 class App extends React.PureComponent {
   render() {
     return (
     <Container className="container">
-      
+      <Switch>
+        <Route path="/">
+
+        </Route>
+
+
+      </Switch>
+
+      <Redirect to="/"/>
     </Container>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,24 @@
-import {createReduxStore} from './redux/store'
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {Provider} from 'react-redux';
+import { createReduxStore } from "./redux/store";
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter } from "react-router-dom";
 
 const store = createReduxStore();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-    <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 serviceWorker.unregister();
